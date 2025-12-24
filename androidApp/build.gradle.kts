3db672453467482fb238dc8a6f580aa3bdea9480
DamienLove <me@damiennichols.com>
2025-12-23 18:14:51 -0700

    chore: release v72 bundles

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -209,8 +209,8 @@ android {
         applicationId = "com.pulselink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 69
-        versionName = "69"
+        versionCode = 72
+        versionName = "72"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
