f1136dcd2f3cf8438bb42d54085ba895f3d4f45b
DamienLove <me@damiennichols.com>
2025-12-26 01:09:14 -0700

    chore: bump version to 84

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -209,8 +209,8 @@ android {
         applicationId = "com.pulselink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 83
-        versionName = "83"
+        versionCode = 84
+        versionName = "84"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
