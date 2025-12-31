1e4b82ec4cb930aca587e253b1551476df9ea47c
DamienLove <me@damiennichols.com>
2025-12-30 18:27:24 -0700

    chore: bump android version to 94

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -221,8 +221,8 @@ android {
         applicationId = "com.pulselink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 93
-        versionName = "93"
+        versionCode = 94
+        versionName = "94"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resValue("string", "google_maps_key", mapsApiKey)
