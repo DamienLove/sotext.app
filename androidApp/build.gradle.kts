4b0dafb5f84de89e5fb28a6e33a022acde9ce7dc
DamienLove <me@damiennichols.com>
2026-01-04 15:28:54 -0700

    chore: release v111 and build

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -221,8 +221,8 @@ android {
         applicationId = "com.pulselink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 110
-        versionName = "110"
+        versionCode = 111
+        versionName = "111"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resValue("string", "google_maps_key", mapsApiKey)
