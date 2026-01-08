58256b5eadc8b733e362a0645aaea8982fa2f824
DamienLove <me@damiennichols.com>
2026-01-08 06:33:46 -0700

    chore: bump PulseLink to 123 and ship

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -221,8 +221,8 @@ android {
         applicationId = "com.pulselink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 122
-        versionName = "122"
+        versionCode = 123
+        versionName = "123"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resValue("string", "google_maps_key", mapsApiKey)
