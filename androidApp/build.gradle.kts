d3ef4613bd00d5424fc5ae4e5a610f6621d7affa
DamienLove <me@damiennichols.com>
2026-01-11 05:05:55 -0700

    chore: release pulselink v129

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -221,8 +221,8 @@ android {
         applicationId = "com.pulselink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 128
-        versionName = "128"
+        versionCode = 129
+        versionName = "129"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resValue("string", "google_maps_key", mapsApiKey)
